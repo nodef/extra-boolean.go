ef86553f11115782b7ab403e425527e95b558994
Subhajit Sahu <wolfram77@gmail.com>
2021-03-24 12:28:40 +0530

    :microbe: try comment ident 2

--- Boolean.go
+++ Boolean.go
@@ -7,20 +7,20 @@ package boolean
 
 import "regexp"
 
-//  PARSE
+// PARSE
 
 // Converts string to boolean.
 //
-//  // Parse(s)
-//  // s: a string
+//   // Parse(s)
+//   // s: a string
 //
 // Example:
-//  Parse("1")        == true
-//  Parse("truthy")   == true
-//  Parse("not off")  == true
-//  Parse("not true") == false
-//  Parse("inactive") == false
-//  Parse("disabled") == false
+//   Parse("1")        == true
+//   Parse("truthy")   == true
+//   Parse("not off")  == true
+//   Parse("not true") == false
+//   Parse("inactive") == false
+//   Parse("disabled") == false
 func Parse(s string) bool {
 	var rfalse = regexp.MustCompile(`(negati|never|refus|wrong|fal|off)|\b(f|n|0)\b`)
 	var rnegate = regexp.MustCompile(`\b(nay|nah|no|dis|un|in)`)
