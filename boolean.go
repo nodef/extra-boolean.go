a4a198bdd904f452a9935ff0d64a1ebde80802be
Subhajit Sahu <wolfram77@gmail.com>
2021-03-29 09:50:13 +0530

    :microbe: w/ doc.go

--- boolean.go
+++ boolean.go
@@ -1,8 +1,3 @@
-// Boolean data type has two possible truth values to represent logic.
-//
-// 📦 Package: https://pkg.go.dev/github.com/golangf/extra-boolean
-//
-// 📘 Wiki: https://github.com/golangf/extra-boolean/wiki.
 package boolean
 
 import (
---
+++ doc.go
@@ -0,0 +1,6 @@
+// Boolean data type has two possible truth values to represent logic.
+//
+// 📦 Package: https://pkg.go.dev/github.com/golangf/extra-boolean
+//
+// 📘 Wiki: https://github.com/golangf/extra-boolean/wiki.
+package boolean
