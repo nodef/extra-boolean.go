bcbcc0f3c5f77f1de2d59e64980bcc56091d931c
Subhajit Sahu <wolfram77@gmail.com>
2021-03-29 09:43:51 +0530

    :microbe: w/ better Parse()

--- boolean.go
+++ boolean.go
@@ -5,7 +5,24 @@
 // 📘 Wiki: https://github.com/golangf/extra-boolean/wiki.
 package boolean
 
-import "regexp"
+import (
+	"regexp"
+	"strconv"
+)
+
+var rNum *regexp.Regexp
+var rTru *regexp.Regexp
+var rFal *regexp.Regexp
+var rNeg *regexp.Regexp
+
+
+func init() {
+  rNum = regexp.MustCompile(`(?i)^[-+]?\d+$`)
+  rTru = regexp.MustCompile(`(?i)\b(?:t|y|1)\b|\b(?:\+|ay|go|on|up)|(?:tru|acc|asc|day|for|hot|inc|joy|new|pos|top|win|yes|dawn|full|safe|grow|high|just|real|some|know|live|love|open|pure|shin|warm|wis[de]|activ|admit|advan|agree|begin|brigh|build|creat|early|enter|float|f(?:i|ou)nd|grant|light|north|prett|prese|publi|start|succe|victr)`)
+  rFal = regexp.MustCompile(`(?i)\b(?:f|n|0)\b|(?:fal|off|dim|end|low|old|back|cold|cool|dark|dead|decr|desc|dirt|down|dull|dusk|exit|late|sink|ugly|absen|botto|close|finis|night|priva|south|wrong)`)
+  rNeg = regexp.MustCompile(`(?i)\b(?:-|na|no|un|in|aft|bad|dis|lie|non|ben[dt]|den[iy]|empt|fail|fake|hate|los[es]|stop|decli|defea|destr|never|negat|refus|rejec|forget|shr[iu]nk|against|is.?nt|can.?(?:no)?t)|(?:hind)`)
+}
+
 
 // PARSE
 
@@ -15,18 +32,24 @@ import "regexp"
 //   // s: a string
 //
 // Example:
-//   Parse("1")        == true
-//   Parse("truthy")   == true
-//   Parse("not off")  == true
-//   Parse("not true") == false
-//   Parse("inactive") == false
-//   Parse("disabled") == false
+//   Parse("1")            == true
+//   Parse("truthy")       == true
+//   Parse("Not Off")      == true
+//   Parse("Not Inactive") == true
+//   Parse("cold")         == false
+//   Parse("inactive")     == false
+//   Parse("Negative Yes") == false
+//   Parse("Negative Aye") == false
 func Parse(s string) bool {
-  var rfalse = regexp.MustCompile(`(negati|never|refus|wrong|fal|off)|\b(f|n|0)\b`)
-  var rnegate = regexp.MustCompile(`\b(nay|nah|no|dis|un|in)`)
-  var f = rfalse.MatchString(s)
-  var n = len(rnegate.FindAllStringIndex(s, -1))%2 == 1
-  return f == n
+  if rNum.MatchString(s) {
+    if n, err := strconv.Atoi(s); err == nil {
+      return n > 0
+    }
+  }
+  t := rTru.MatchString(s)
+  f := rFal.MatchString(s)
+  n := len(rNeg.FindAllStringIndex(s, -1)) % 2 == 1
+  return Nimply(f, t) == n
 }
 
 
