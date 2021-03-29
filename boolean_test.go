0bee3a80cdbac23d6035713aa6945d951197ea28
Subhajit Sahu <wolfram77@gmail.com>
2021-03-29 09:48:25 +0530

    :microbe: w/ better Parse()

--- boolean_test.go
+++ boolean_test.go
@@ -5,16 +5,20 @@ import "fmt"
 // PARSE
 
 func ExampleParse() {
-  fmt.Println(Parse("1"))        // true
-  fmt.Println(Parse("truthy"))   // true
-  fmt.Println(Parse("not off"))  // true
-  fmt.Println(Parse("not true")) // false
-  fmt.Println(Parse("inactive")) // false
-  fmt.Println(Parse("disabled")) // false
+  fmt.Println(Parse("1")           ) // true
+  fmt.Println(Parse("truthy")      ) // true
+  fmt.Println(Parse("Not Off")     ) // true
+  fmt.Println(Parse("Not Inactive")) // true
+  fmt.Println(Parse("cold")        ) // false
+  fmt.Println(Parse("inactive")    ) // false
+  fmt.Println(Parse("Negative Yes")) // false
+  fmt.Println(Parse("Negative Aye")) // false
   // Output:
   // true
   // true
   // true
+  // true
+  // false
   // false
   // false
   // false
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/golangf/extra-boolean
 
 // name: extra-boolean
-// version: v1.0.3
+// version: v1.0.4
 
 go 1.16
